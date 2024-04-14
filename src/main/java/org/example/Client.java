aa37f0d34a48079009b53a48a16c61d69e234a6d
Harjappan Singh <singhharjappan@gmail.com>
2024-04-14 20:33:48 +0100

    Defined logic for client side to create a socket and get input from user and then sends to server, which will then process it and sends a response back

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
@@ -7,9 +10,15 @@ public class Client {
         try{
             System.out.println("Client has started");
             Socket soc = new Socket("localhost", 8888);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter a number: ");
+            int userNumber = Integer.parseInt(userInput.readLine());
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(userNumber);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
         } catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,9 +14,22 @@ public class Server {
             ServerSocket ssocket = new ServerSocket(8888);
             Socket soc = ssocket.accept();
             System.out.println("A client has connected and connection has been established");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            int userNum = Integer.parseInt(in.readLine());
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            int response = calculateFactorial(userNum);
+            out.println("The factorial of " + userNum + " is: " + response);
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
 
+    static int calculateFactorial(int number){
+        int fact = 1;
+        for(int i=1;i<=number; i++){
+            fact = fact*i;
+        }
+        return fact;
     }
 }
